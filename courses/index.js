8639565f1eac729d89a838c85c8a643d42d125c0
Mr. Metelus <Mdmetelus@gmail.com>
2019-01-10 03:12:22 -0500

    "added another get route, and some error fixes"

--- courses/index.js
+++ courses/index.js
@@ -2,38 +2,52 @@ const express = require('express');
 const helmet = require('helmet');
 const knex = require('knex');
 
-const knexConfig = require('./knexfile');
+const knexConfig = require('./knexfile.js');
+
 const db = knex(knexConfig.development);
 
-const server = express();
-const PORT = 54321
-;
+const PORT = 54321;
 
+const server = express();
 
 server.use(helmet());
 server.use(express.json());
 
 
 server.get('/', (req, res) => {
-    res.send(`Its Alive`);
+    res.send(`It's Alive!`);
+});
+
+
+/// get route /// a list of courses currently on the database
+
+server.get('/api/courses', (req, res)  => {
+    db('courses').then( courses => {
+        res.status(200).json(courses);
+    })
+    .catch( err => {
+        res.status(500).json(err);
+    })
 });
 
 /// CREATE COURSES ---------------------------
-server.post('/api/courses', (req,res) => {
+server.post('/api/courses', (req, res) => {
     /// grab the date from the body
     const course = req.body;
 
     /// save data to the database
-    debug.insert(course).into('courses')
+    db.insert(course)
+        .into('courses')
         .then( ids => {
-            res,status(201).json(ids)
+            /// return the id of newly created record
+            res.status(201).json(ids[0]); /// or json(ids); /// for a list of all the the names in the aray
+            console.log(ids);
         })
         .catch( err => {
             res.status(500).json(err);
         });
 
 
-    /// return the id of newly created record
 });
 
 
