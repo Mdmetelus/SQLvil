8dc7ef5e516712fe8dc584032507927a0bdfd97f
Mr. Metelus <Mdmetelus@gmail.com>
2019-01-10 02:40:42 -0500

    "..."

--- courses/index.js
+++ courses/index.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const helmet = require('helmet');
+const knex = require('knex');
 
+const knexConfig = require('./knexfile');
+const db = knex(knexConfig.development);
 
 const server = express();
-const PORT = 5432;
+const PORT = 54321
+;
 
 
 server.use(helmet());
@@ -14,7 +18,46 @@ server.get('/', (req, res) => {
     res.send(`Its Alive`);
 });
 
+/// CREATE COURSES ---------------------------
+server,post('/api/courses', (req,res) => {
+    /// grab the date from the body
+    const course = request.body;
+
+
+
+    /// save data to the database
+    debug.unsert(course).into(courses)
+        .then()
+        .catch()
+
+
+    /// return the id of newly created record
+})
+
+
+
+///UPDATE COURSES -----------------------------
+
+// server.update('/api/courses' (req, res) => {
+  
+// });
+
+
+
+////DELETE COURSE -----------------------------
+
+// server.delete('/api/courses' (req, res) => {
+  
+// });
+
+
+
+////GET COURSE
+
+
+
+
 server.listen(PORT, () => {
     console.log(`Its Working! Its Working!
-                API Running on Port ${5432}`);
+        API Running on Port ${PORT} !`);
 })
