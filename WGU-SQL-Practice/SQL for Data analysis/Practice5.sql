ab913fb462e6f81a6df4a7bc842e9941d9e378b4
Mr.Metelus <mdm@blue42.home>
2019-05-25 01:14:26 -0400

    CREATING THE DATABASE AND CREATEING TABLES

---
+++ WGU-SQL-Practice/SQL for Data analysis/Practice5.sql
@@ -0,0 +1,30 @@
+-- CREATING THE DATABASE
+-- ANSI SQL standards define a command to create a database schema:
+
+CREATE SCHEMA AUTHORIZATION {creator};
+
+-- if the creator is MAX, the following command is used:
+
+CREATE SCHEMA AUTHORIZATION MAX;
+
+
+-- Creating Table Structures:
+
+CREATE TABLE newtablename (
+colomn1 data type [contraint] [,
+colomn2 data type [constraint] ] [,
+PRIMANRY KEY (column1 [,colomn2]) ] [,
+FOREIGN KEY (colomn1 [,colomn2]) REFERENCES tablename]
+CONSTRAINT constraint ] );
+
+-- Creating Table Structures:
+
+CREATE TABLE VENDOR (
+V_CODE INTEGER NOT NULL UNIQUE,
+V_NAME VARCHAR(35)NOT NULL,
+V_CONTACT VARCHAR(25) NOT NULL,
+V_AREACODE CHAR(3) NOT NULL,
+V_PHONE CHAR(8) NOT NULL,
+V_STATE CHAR(2) NOT NULL,
+V_ORDER CHAR(1) NOT NULL,
+PRIMANRY KEY (V_CODE));
