db147c0156342df01343241a2d24623f346f58db
Mr.Metelus <mdm@blue42.home>
2019-05-29 19:53:38 -0400

    more select pratice

--- WGU-SQL-Practice/SQL for Data analysis/Practice8.sql
+++ WGU-SQL-Practice/SQL for Data analysis/Practice8.sql
@@ -1,2 +1,25 @@
+-- Selec Statment
+
 SELECT population FROM world
   WHERE name = 'Germany';
+
+SELECT * FROM table;
+
+SELECT * FROM Album; -- this is a comment
+
+/* this is also a comment, a c style comment*/
+
+SELECT *  FROM Album WHERE Label = 'Columbia';
+
+SELECT COUNT(*) FROM Album WHERE Label = 'Columbia';
+
+SELECT Name, Population / 1000000 AS PopMM
+    FROM Country
+    WHERE Population >= 1000000
+    ORDER BY Population DESC;
+
+-- Select rows
+
+
+-- Select columns
+
