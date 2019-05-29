43f6e4ff0e234679c62cf215172dd62fa98eeef2
Mr.Metelus <mdm@blue42.home>
2019-05-29 17:01:53 -0400

    update

--- WGU-SQL-Practice/SQL for Data analysis/Practice7.sql
+++ WGU-SQL-Practice/SQL for Data analysis/Practice7.sql
@@ -5,3 +5,11 @@ SELECT DISTINCT subject
 
  -- SELECT subject FROM nobel Where subject = ;
  -- SELECT COUNT FROM nobel
+
+ SELECT subject, winner, yr FROM nobel WHERE subject = 'Physics'; 
+
+ SELECT COUNT(winner) FROM nobel
+ WHERE subject = 'Physics'; 
+
+ Select subject, COUNT(winner) From nobel
+GROUP BY subject;
