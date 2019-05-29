ad5d77d7e1b4459674b8a51712967d1feea1ac0d
Mr.Metelus <mdm@blue42.home>
2019-05-29 17:20:58 -0400

    first 5 completed

--- WGU-SQL-Practice/SQL for Data analysis/Practice7.sql
+++ WGU-SQL-Practice/SQL for Data analysis/Practice7.sql
@@ -1,15 +1,28 @@
+-- Show the total number of prizes awarded.
+
 SELECT COUNT(winner) FROM nobel;
 
+-- List each subject - just once
 SELECT DISTINCT subject
  FROM nobel;
 
  -- SELECT subject FROM nobel Where subject = ;
  -- SELECT COUNT FROM nobel
 
+
  SELECT subject, winner, yr FROM nobel WHERE subject = 'Physics'; 
 
+-- Show the total number of prizes awarded for Physics.
  SELECT COUNT(winner) FROM nobel
  WHERE subject = 'Physics'; 
 
  Select subject, COUNT(winner) From nobel
 GROUP BY subject;
+
+-- For each subject show the subject and the number of prizes.
+Select subject, COUNT(winner) From nobel
+GROUP BY subject 
+ORDER BY subject DESC;
+
+-- ORDER BY subject DESC
+-- WHERE winner = 1;
