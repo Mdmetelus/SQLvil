d03b6a9b3c9d4cd27795906d4a8548dbadd6d2a4
Mdmetelus <Mdmetelus@gmail.com>
2019-06-01 22:38:13 -0400

    simple insert statement and create table statment

--- WGU-SQL-Practice/SQL for Data analysis/Practice9.sql
+++ WGU-SQL-Practice/SQL for Data analysis/Practice9.sql
@@ -1,3 +1,7 @@
 CREATE TABLE test ( a INT, b TEXT, c TEXT );
 INSERT INTO test VALUES( 1, 'This', 'Right Here!');
 SELECT * FROM test;
+
+
+INSERT INTO test ( b, c ) VALUES ( 'That', 'Over there!' )
+SELECT * FROM test
