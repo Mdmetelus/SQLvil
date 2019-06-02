c225ffae326a5ed52ba7d22e8a09c8c3dbb1aaa9
Mdmetelus <Mdmetelus@gmail.com>
2019-06-02 00:08:36 -0400

    ...

--- WGU-SQL-Practice/SQL for Data analysis/Practice11.sql
+++ WGU-SQL-Practice/SQL for Data analysis/Practice11.sql
@@ -7,4 +7,10 @@ SELECT * FROM test;
 
 SELECT * FROM test WHERE a = NULL;
 -- THIS WILL GET NO RESULT BECAUSE ALL THE CREATE SPOTS IN THE TABLE CURRENTLY HAVE VALUES.
+SELECT * FROM test WHERE a IS NULL;
 
+SELECT * FROM test WHERE a IS NOT NULL; -- thi gives you avery row inthe table with a value.
+
+DELETE FORM test; -- entire row deleted
+
+INSERT INTO test ( a, b, c ) VALUES ( 0, NULL, '' );
