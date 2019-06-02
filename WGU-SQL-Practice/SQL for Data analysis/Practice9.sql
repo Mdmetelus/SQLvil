44899578b498136b26f7f80701acd8c3ae3e2acd
Mdmetelus <Mdmetelus@gmail.com>
2019-06-01 22:43:36 -0400

    onother type of insert

--- WGU-SQL-Practice/SQL for Data analysis/Practice9.sql
+++ WGU-SQL-Practice/SQL for Data analysis/Practice9.sql
@@ -6,3 +6,7 @@ SELECT * FROM test;
 INSERT INTO test ( b, c ) VALUES ( 'That', 'Over there!' )
 SELECT * FROM test;
 -- there will be a null in the  a column.
+
+INSERT INTO test ( a, b, c ) SELECT id, name , description FROM item;
+-- taking the three values fron the item table row and inserting them into the test table 
+SELECT * FROM test;
