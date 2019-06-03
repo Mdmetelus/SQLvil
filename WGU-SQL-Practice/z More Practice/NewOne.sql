37bc290c3a0283b952fc3330c592f8ced124abd3
Mdmetelus <Mdmetelus@gmail.com>
2019-06-02 20:20:45 -0400

    partial implimentation of create purchases table

--- WGU-SQL-Practice/z More Practice/NewOne.sql
+++ WGU-SQL-Practice/z More Practice/NewOne.sql
@@ -9,5 +9,17 @@ CREATE TABLE users (
     address VARCHAR(255),
     zip CHAR(10),
     phoneNum CHAR(15),
-    accountNum CHAR(14)
+    accountNum CHAR(14),
+    firstName VARCHAR(255),
+    lastname VARCHAR(255)
+);
+
+CREATE TABLE purchaces (
+    id INTEGER,
+    purchaseNum VARCHAR(255),
+    item VARCHAR(255),
+    cost, -- NOT AN INT, IT SHOULD BE BASED ON A MONETARY AMOUNT.
+    location VARCHAR(255),
+    customerNum, -- accountNum (foreign key, connect to the users table )
+    time -- ?
 );
