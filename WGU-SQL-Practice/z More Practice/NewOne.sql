75e55147ba3a573d8edaa7e51025efa3ea60775f
Mdmetelus <Mdmetelus@gmail.com>
2019-06-02 22:33:26 -0400

    updates

--- WGU-SQL-Practice/z More Practice/NewOne.sql
+++ WGU-SQL-Practice/z More Practice/NewOne.sql
@@ -11,15 +11,16 @@ CREATE TABLE users (
     phoneNum CHAR(15),
     accountNum CHAR(14),
     firstName VARCHAR(255),
-    lastname VARCHAR(255)
+    lastName VARCHAR(255),
+    gender CHAR(1)
 );
 
 CREATE TABLE purchaces (
     id INTEGER,
     purchaseNum VARCHAR(255),
     item VARCHAR(255),
-    cost, -- NOT AN INT, IT SHOULD BE BASED ON A MONETARY AMOUNT.
-    location VARCHAR(255),
+    purchaseCost, -- NOT AN INT, IT SHOULD BE BASED ON A MONETARY AMOUNT.
+    purchaseLocation VARCHAR(255),
     customerNum, -- accountNum (foreign key, connect to the users table )
-    time -- ?
+    purchaseTime -- ?
 );
