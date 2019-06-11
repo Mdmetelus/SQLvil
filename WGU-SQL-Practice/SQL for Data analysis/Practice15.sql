a3fc1b0f310fea4eb9e3dcf6854953e39e2abe4c
Mdmetelus <Mdmetelus@gmail.com>
2019-06-11 19:52:23 -0400

    ...

--- WGU-SQL-Practice/SQL for Data analysis/Practice15.sql
+++ WGU-SQL-Practice/SQL for Data analysis/Practice15.sql
@@ -7,3 +7,12 @@ CREATE TABLE client (
     name TEXT
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+INSERT INTO CLIENT ( name ) VALUES ( 'Freddy' );
+INSERT INTO CLIENT ( name ) VALUES ( 'Karen' );
+INSERT INTO CLIENT ( name ) VALUES ( 'Harry' );
+SELECT * FROM client;
+
+CREATE TABLE book (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title TEXT
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
