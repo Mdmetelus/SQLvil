0702ce435324f20f31679bd06de021e1b49c246e
Mdmetelus <Mdmetelus@gmail.com>
2019-06-14 23:42:20 -0400

    update

--- WGU-SQL-Practice/SQL for Data analysis/Practice14.sql
+++ WGU-SQL-Practice/SQL for Data analysis/Practice14.sql
@@ -9,3 +9,12 @@ INSERT INTO test ( a, b ) VALUES ( 'NEW VALUE', 'NEW VALUE 2' );
 
 -- serial  shorthand for :
 -- big int, unsigned, not null, auto increment, unique;
+
+DROP TABLE IF EXISTS newtable;
+
+CREATE TABLE newtable(
+    id INTEGER AUTO_INCREMENT PRIMARY KEY, -- id SERIAL means the same but takes up more space.
+    a VARCHAR(255),
+    b VARCHAR(255),
+    timeOfEvent  TIMESTAMP DEFAULT NOW(),
+);
