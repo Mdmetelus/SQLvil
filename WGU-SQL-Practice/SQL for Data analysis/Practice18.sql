c41f1ad9bc3795a17c2dfa798ae36080fa75a117
Mdmetelus <Mdmetelus@gmail.com>
2019-06-23 03:53:05 -0400

    counts practice

--- WGU-SQL-Practice/SQL for Data analysis/Practice18.sql
+++ WGU-SQL-Practice/SQL for Data analysis/Practice18.sql
@@ -1,5 +1,13 @@
 SELECT COUNT(*) FROM Country;
--- COUNT, counts all the non null values in the country table
+-- COUNT, counts all the non null values in the country table;
 
 
 SELECT COUNT (Population) FROM Country;
+-- COUNT, counts all the non null values for population  in the Country table;
+
+SELECT Continent, COUNT(*) FROM Country GROUP BY Continent;
+
+SELECT Continent, COUNT(*) AS Count FROM Country GROUP BY Continent ORDER BY Count DESC;
+-- list the continents and te country count for each continent from the most to the least number of countries.
+
+SELECT COUNT(DISTINCT Continent) FROM Country;
