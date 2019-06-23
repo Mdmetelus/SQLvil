d354ad8dcfa36c2215af1938de8281ee843d5e4b
Mdmetelus <Mdmetelus@gmail.com>
2019-06-23 04:20:08 -0400

    ...

--- WGU-SQL-Practice/SQL for Data analysis/Practice17.sql
+++ WGU-SQL-Practice/SQL for Data analysis/Practice17.sql
@@ -17,3 +17,11 @@ INSERT INTO widgetInventory ( description, onhand ) VALUES ( 'paper', 25 );
 INSERT INTO widgetInventory ( description, onhand ) VALUES ( 'scissors', 25 );
 
 SELECT * FROM widgetInventory;
+
+START TRANSACTION;
+INSERT INTO widgetSales ( inv_id, quan, price ) VALUES ( 1, 5, 500 );
+UPDATE widgetInventory SET onhand = ( onhand - 5 ) WHERE id = 1;
+COMMIT;
+
+
+
