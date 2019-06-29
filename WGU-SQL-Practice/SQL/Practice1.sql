ec965f7c7f4da7463d97c6002ce807bbb4cb3200
Mdmetelus <Mdmetelus@gmail.com>
2019-06-28 23:57:50 -0400

    update

--- WGU-SQL-Practice/SQL/Practice1.sql
+++ WGU-SQL-Practice/SQL/Practice1.sql
@@ -7,3 +7,11 @@ SHOW COLOMNS FROM customers;
 SHOW COLOMNS FROM CUSTOMERS;
 
 SELECT name FROM customers;
+
+SELECT FirstName FROM customers;
+
+SELECT City FROM customers;
+
+SELECT FirstName, LastName, City FROM customers;
+
+SELECT name, city From people;
