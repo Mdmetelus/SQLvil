dfeb51368453a972216c0a0e225b5c8b9c3a5b31
Mdmetelus <Mdmetelus@gmail.com>
2019-06-29 03:23:45 -0400

    update

--- WGU-SQL-Practice/SQL for Data analysis/Practice19.sql
+++ WGU-SQL-Practice/SQL for Data analysis/Practice19.sql
@@ -1,1 +1,17 @@
 SHOW DATABASES;
+
+SHOW TABLES FROM informaton_schema;
+
+USE informaton_schema;
+
+SHOW TABLES;
+
+USE informaton_schema;
+
+SHOW COLOMNS FROM SCHEMATA;
+
+USE mysql;
+
+SHOW COLOMNS FROM SCHEMATA;
+
+SHOW COLOMNS FROM informaton_schema.SCHEMATA;
