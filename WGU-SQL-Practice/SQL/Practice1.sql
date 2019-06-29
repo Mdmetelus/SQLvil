8e00add4d2883958c17497753886796ecd017ef4
Mdmetelus <Mdmetelus@gmail.com>
2019-06-29 00:10:42 -0400

    limits

--- WGU-SQL-Practice/SQL/Practice1.sql
+++ WGU-SQL-Practice/SQL/Practice1.sql
@@ -32,3 +32,10 @@ SELECT ID, LastName FROM customers LIMIT 20;
 
 SELECT name FROM students LIMIT 7;
 
+SELECT ID, FirstName, LastName, City From customers LIMIT 3, 4;
+
+SELECT ID, FirstName, LastName, City From customers LIMIT 10, 20;
+
+SELECT ID, FirstName, LastName, City From cutomers LIMIT 7, 3;
+
+SELECT id, name FROM customers LIMIT 4, 12;
