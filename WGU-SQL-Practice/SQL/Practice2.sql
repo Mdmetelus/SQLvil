87f4f9db4621453f463b8ad22aeb6a5cf0da1e4e
Mdmetelus <Mdmetelus@gmail.com>
2019-06-30 04:55:05 -0400

    update

---
+++ WGU-SQL-Practice/SQL/Practice2.sql
@@ -0,0 +1,11 @@
+SELECT column_one FROM table_One WHERE condition = 1;
+
+SELECT * FROM customers WHERE ID = 7;
+
+SELECT id, Name FROM students WHERE id = 23;
+
+SELECT * FROM customers WHERE ID != 5;
+
+SELECT * FROM customers WHERE ID IS NOT NULL;
+
+SELECT id, name FROM students WHERE id >= 12;
