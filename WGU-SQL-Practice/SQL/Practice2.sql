7f5093e93db8cd1170fcf9f7885adcce704d10ef
Mdmetelus <Mdmetelus@gmail.com>
2019-06-30 05:06:03 -0400

    select where practice

--- WGU-SQL-Practice/SQL/Practice2.sql
+++ WGU-SQL-Practice/SQL/Practice2.sql
@@ -9,3 +9,11 @@ SELECT * FROM customers WHERE ID != 5;
 SELECT * FROM customers WHERE ID IS NOT NULL;
 
 SELECT id, name FROM students WHERE id >= 12;
+
+SELECT column_one FROM table_One WHERE column_one BETWEEN value1 AND value2;
+
+SELECT * FROM customers WHERE ID BETWEEN 3 AND 7;
+
+SELECT id, name FROM students WHERE id BETWEEN 13 AND 45;
+
+SELECT ID, FirstName, LastName, City FROM  customers WHERE City = 'New York';
