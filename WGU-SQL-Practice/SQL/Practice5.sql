0f3dda396609cdb4a211ad2aea7635054f84c4bf
Mdmetelus <Mdmetelus@gmail.com>
2019-07-02 03:35:12 -0400

    Update done

--- WGU-SQL-Practice/SQL/Practice5.sql
+++ WGU-SQL-Practice/SQL/Practice5.sql
@@ -1,6 +1,16 @@
- -- Update and Delete statments practice;
+ -- Updating and Deleting statments practice;
 
 UPDATE table_name
 SET column1=value1, column2= value2
 Where condition IS NOT NULL;
+
+UPDATE Employees
+SET Salary = 5000 WHERE ID = 1;
+
+UPDATE students SET universities = 'Stanford' WHERE name = 'John';
+
+UPDATE Employees
+SET Salary = 5000, FirstName = 'Robert' WHERE ID = 1;
+
+UPDATE students SET name = 'Peter', age = 32 WHERE id = 147;
 
