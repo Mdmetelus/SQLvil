be296f6056df6a38fd71a426fd77681e42c0696f
Mdmetelus <Mdmetelus@gmail.com>
2019-07-02 10:08:04 -0400

    update

--- WGU-SQL-Practice/SQL for Data analysis/Practice19.sql
+++ WGU-SQL-Practice/SQL for Data analysis/Practice19.sql
@@ -18,7 +18,38 @@ SHOW COLOMNS FROM informaton_schema.SCHEMATA;
 
 SHOW COLOMNS FROM mysql.help_topics;
 
-SHOW
+SHOW COLOMNS FROM informaton_schema.GLOBAL_VARIABLES;
 
+SHOW COLOMNS FROM informaton_schema.SCHEMATA;
 
 CREATE DATABASE my_database;
+
+SHOW COLOMNS FROM informaton_schema.COLOMNS;
+
+USE myql;
+
+SHOW COLUMNS FORM help_topic;
+
+SHOW COLOMNS FROM slow_log;
+
+SHOW COLOMNS FROM informaton_schema;
+
+CREATE DATABASE my_new_database;
+
+CREATE DATABASE IF NOT EXIST my_new_database;
+
+SHOW WARNINGS;
+
+CREATE DATABASE IF NOT EXIST another_new_db DEFAULT CHARACTER SET utf8;
+
+SHOW COLLATION LIKE 'latin1%';
+
+DROP DATABASE IF EXIST another_new_db
+
+SHOW WARNINGS;
+
+DROP DATABASE another_new_db;
+
+SHOW CREATE TABLE;
+
+SHOW CREATE TABLEE help_category/g -- /g same as;
