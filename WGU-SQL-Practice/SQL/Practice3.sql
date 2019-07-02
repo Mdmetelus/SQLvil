cd706f22fc085de2ded8dcff1df18147b64383d4
Mdmetelus <Mdmetelus@gmail.com>
2019-07-01 23:18:50 -0400

    ubqueries begin

--- WGU-SQL-Practice/SQL/Practice3.sql
+++ WGU-SQL-Practice/SQL/Practice3.sql
@@ -13,3 +13,13 @@ SELECT AVG(Salary) As avg_salary FROM employees;
 SELECT AVG(cost) FROM items;
 
 SELECT SUM(Salary) FROM employees;
+
+-- Functions and Subqueries
+
+SELECT AVG(Salary) FROM employees;
+
+SELECT FirstName, Salary FROM employees WHERE Salary > 3100 ORDER BY Salary DESC;
+
+SELECT LastName, FirstName, Salary FROM employees WHERE Salary > 1000 ORDER BY Salary, LastName, FirstName DESC;
+
+SELECT * FROM items WHERE cost > 463 ORDER BY cost DESC;
