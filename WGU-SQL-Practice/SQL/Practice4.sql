386d1ec3677e02be893dded1456e4830877ca1a8
Mdmetelus <Mdmetelus@gmail.com>
2019-07-02 01:21:38 -0400

    upate

--- WGU-SQL-Practice/SQL/Practice4.sql
+++ WGU-SQL-Practice/SQL/Practice4.sql
@@ -15,3 +15,14 @@ SELECT ct.ID, ct.Name, ord.Name, ORD.Amount
 FROM customers AS ct, orders AS ord
 WHERE ct.ID = ord.Customers_ID ORDER BY ct.ID;
 
+SELECT ct.name, it.name 
+FROM customers AS ct ,items AS it
+WHERE it.seller_id = ct.id;
+
+SELECT colomn_one 
+FROM table_one INNER JOIN table_two
+ON  table_one.colomn_one = table_two.colomn_one;
+
+SELECT students.name, universities.name 
+FROM students, universities
+WHERE stuents.university_id = universities.id;
