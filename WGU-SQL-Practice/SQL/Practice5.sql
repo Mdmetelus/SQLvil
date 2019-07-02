b22f9227a80618343eea846f19bfb14807000b45
Mdmetelus <Mdmetelus@gmail.com>
2019-07-02 04:21:31 -0400

    createing tables

--- WGU-SQL-Practice/SQL/Practice5.sql
+++ WGU-SQL-Practice/SQL/Practice5.sql
@@ -23,3 +23,27 @@ DELETE FROM Employees WHERE ID = 1;
 DELETE FROM people WHERE id > 5 AND id < 10;
 
  -- Creating a new table practice;
+
+CREATE TABLE;
+
+CREATE TABLE (
+    ID serial,
+    some_column varchar (50)
+    
+) 
+
+CREATE TABLE newest_Table (
+    id SERIAL,
+    name VARCHAR(255),
+    age INT(2),
+    catchLine VARCHAR(255)
+
+);
+
+CREATE TABLE users 
+(
+    UserID int,
+    FirstName varchar(100),
+    LastName varchar(100),
+    City varchar(100)
+)
