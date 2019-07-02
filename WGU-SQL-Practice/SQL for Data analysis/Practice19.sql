c4d656c62c2a950cf713f8a34d7762081906e3b5
Mdmetelus <Mdmetelus@gmail.com>
2019-07-02 10:36:07 -0400

    error fixes and some table manipulations

--- WGU-SQL-Practice/SQL for Data analysis/Practice19.sql
+++ WGU-SQL-Practice/SQL for Data analysis/Practice19.sql
@@ -1,12 +1,12 @@
 SHOW DATABASES;
 
-SHOW TABLES FROM informaton_schema;
+SHOW TABLES FROM information_schema;
 
-USE informaton_schema;
+USE information_schema;
 
 SHOW TABLES;
 
-USE informaton_schema;
+USE information_schema;
 
 SHOW COLOMNS FROM SCHEMATA;
 
@@ -14,17 +14,17 @@ USE mysql;
 
 SHOW COLOMNS FROM SCHEMATA;
 
-SHOW COLOMNS FROM informaton_schema.SCHEMATA;
+SHOW COLOMNS FROM information_schema.SCHEMATA;
 
 SHOW COLOMNS FROM mysql.help_topics;
 
-SHOW COLOMNS FROM informaton_schema.GLOBAL_VARIABLES;
+SHOW COLOMNS FROM information_schema.GLOBAL_VARIABLES;
 
-SHOW COLOMNS FROM informaton_schema.SCHEMATA;
+SHOW COLOMNS FROM information_schema.SCHEMATA;
 
 CREATE DATABASE my_database;
 
-SHOW COLOMNS FROM informaton_schema.COLOMNS;
+SHOW COLOMNS FROM information_schema.COLOMNS;
 
 USE myql;
 
@@ -32,7 +32,7 @@ SHOW COLUMNS FORM help_topic;
 
 SHOW COLOMNS FROM slow_log;
 
-SHOW COLOMNS FROM informaton_schema;
+SHOW COLOMNS FROM information_schema;
 
 CREATE DATABASE my_new_database;
 
@@ -52,4 +52,31 @@ DROP DATABASE another_new_db;
 
 SHOW CREATE TABLE;
 
-SHOW CREATE TABLEE help_category/g -- /g same as;
+SHOW CREATE TABLE help_category/g -- \g same as; 
+ -- there is also a line/statment terminator with \G \g ;
+
+CREATE DATABASE addresses;
+
+DROP TABLE IF EXISTS people;
+
+CREATE TABLE people (
+    first_name varchar(50) NOT NULL DEFAULT '',
+    last_name varchar(50) NOT NULL DEFAULT '',
+    mobile varchar(20) NOT NULL DEFAULT '',
+    birthday date DEFAULT NULL,
+    PRIMARY KEY (first_name, last_name, birthday)
+);
+
+ALTER TABLE people DROP PRIMARY KEY, ADD PRIMARY KEY(first_name, last_name, birthday);
+
+SELECT TABLE_NAME 
+FROM information_schema
+WHERE table_shema = 'my_database';
+
+SELECT TABLE COMMENT FROM TABLES;
+
+USE information_schema;
+
+SHOW CREATE TABLE people\g
+
+INSERT INTO people (birthday, first_name, last_name, mobile) VALUES ( '1996-05-27', 'Paul', 'Thompson', '586-7987');
