b2c18fce2918f95d97cfe8e95f6009dbcb706642
Mdmetelus <Mdmetelus@gmail.com>
2019-07-03 06:17:45 -0400

    TEST 2.0 completed.

--- WGU-SQL-Practice/SQL/Practice6.sql
+++ WGU-SQL-Practice/SQL/Practice6.sql
@@ -106,3 +106,7 @@ UPDATE zoo SET food_balance = 23 WHERE age > (SELECT AVG(age) FROM zoo);
 SELECT customers.name, cities.name 
 FROM customers RIGHT OUTER JOIN cities
 ON cities.id = customers.city_id;
+
+ALTER TABLE students ADD last_name Varchar(100);
+
+SELECT name FROM students WHERE university IN ('MIT', 'Stanford', 'Harvard') AND name = 'Jake';
