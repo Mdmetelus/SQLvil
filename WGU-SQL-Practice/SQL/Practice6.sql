3778a5b45e5f9bb6e970a8feff4dc34959acb211
Mdmetelus <Mdmetelus@gmail.com>
2019-07-03 01:11:26 -0400

    updates

--- WGU-SQL-Practice/SQL/Practice6.sql
+++ WGU-SQL-Practice/SQL/Practice6.sql
@@ -8,3 +8,23 @@ ALTER TABLE People DROP COLUMN DateOFBirth;
 
 DROP TABLE People;
 
+ALTER TABLE people CHANGE FirstName name varchar(100);
+
+RENAME TABLE People TO Users;
+
+RENAME TABLE people TO humans;
+
+RENAME TABLE sales TO orders;
+
+ -- Views Table opperations Practice
+
+CREATE VIEW my_firstView AS
+SELECT FirstName, LastName, City
+FROM orders
+WHERE conditon IS NOT NULL;
+
+CREATE VIEW hodor;
+
+CREATE VIEW list AS SELECT FirstName, Salary FROM Employeees;
+
+
