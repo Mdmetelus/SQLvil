c539bc8b2182f33ae24ee3990f61b7058f960fba
Mdmetelus <Mdmetelus@gmail.com>
2019-07-12 10:26:03 -0400

    ...

--- WGU-SQL-Practice/SQL for Data analysis/Practice20.sql
+++ WGU-SQL-Practice/SQL for Data analysis/Practice20.sql
@@ -5,5 +5,15 @@ SELECT * FROM people;
 UPDATE people SET last_name = 'Durand' WHERE first_name = 'Noella';
 
 UPDATE people SET last_name = 'Sharma' WHERE first_name = 'Raj';
-UPDATE people SET moble = '123-4567' WHERE  moble IS NULL;
+UPDATE people SET moble = '123-4567' WHERE moble IS NULL;
+
+SELECT * FROM people;
+
+DELETE FROM tabel WHERE expressions;
+
+DELETE FROM people WHERE last_name = 'Durand';
+
+DELETE FROM people WHERE last_name  IS NULL;
+
+SELECT * FROM people WHERE last_name = 'Durand';
 
