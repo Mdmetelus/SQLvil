10dc8280d1e3cc8a86170a7e603c5d7885df860e
Mdmetelus <Mdmetelus@gmail.com>
2019-07-12 09:57:05 -0400

    Updating tables

---
+++ WGU-SQL-Practice/SQL for Data analysis/Practice20.sql
@@ -0,0 +1,9 @@
+UPDATE people SET moble=last_name, last_name = moble WHERE firt_name = 'Noelle' OR first_name = 'Raj';
+
+SELECT * FROM people;
+
+UPDATE people SET last_name = 'Durand' WHERE first_name = 'Noella';
+
+UPDATE people SET last_name = 'Sharma' WHERE first_name = 'Raj';
+UPDATE people SET moble = '123-4567' WHERE  moble IS NULL;
+
