c2e58a0a8d98d4496b934c13ae76c3beacc4a9bf
Mdmetelus <Mdmetelus@gmail.com>
2019-07-18 07:01:49 -0400

    update

---
+++ WGU-SQL-Practice/SQL for Data analysis/Practice21.sql
@@ -0,0 +1,12 @@
+
+SELECT last_name, GROUP_CONCAT(moble SEPERATOR ' or ') FROM people WHERE last_name != 'Cabral' GROUP BY last_name HAVING COUNT(*) > 1;
+
+SELECT first_name, birthday FROM people ORDER BY birthday;
+
+SELECT first_name, last_name, birthday FROM people ORDER BY birthday DESC;
+
+SELECT first_name, last_name FROM people ORDER BY last_name, first_name;
+
+SELECT first_name, birthday FROM people ORDER BY birthday DESC LIMIT 5;
+
+SELECT first_name, MONTHNAME(birthday) as mon, birthday FROM people ORDER BY MONTH(birthday);
