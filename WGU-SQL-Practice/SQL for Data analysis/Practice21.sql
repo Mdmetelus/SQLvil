d6059377a7a34462e46ac4e65a2477aa596f9247
Mdmetelus <Mdmetelus@gmail.com>
2019-07-18 07:02:56 -0400

    ...

--- WGU-SQL-Practice/SQL for Data analysis/Practice21.sql
+++ WGU-SQL-Practice/SQL for Data analysis/Practice21.sql
@@ -10,3 +10,7 @@ SELECT first_name, last_name FROM people ORDER BY last_name, first_name;
 SELECT first_name, birthday FROM people ORDER BY birthday DESC LIMIT 5;
 
 SELECT first_name, MONTHNAME(birthday) as mon, birthday FROM people ORDER BY MONTH(birthday);
+
+SELECT last_name, COUNT(*) FROM people GROUP BY last_name;
+
+SELECT last_name, COUNT(*) FROM people GROUP BY last_name ORDER BY NULL;
