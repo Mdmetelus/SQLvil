05c1bb05d7066480d3f00ac876199b7e037dae1a
Mdmetelus <Mdmetelus@gmail.com>
2019-07-19 03:09:08 -0400

    ..

--- WGU-SQL-Practice/SQL for Data analysis/Practice21.sql
+++ WGU-SQL-Practice/SQL for Data analysis/Practice21.sql
@@ -14,3 +14,7 @@ SELECT first_name, MONTHNAME(birthday) as mon, birthday FROM people ORDER BY MON
 SELECT last_name, COUNT(*) FROM people GROUP BY last_name;
 
 SELECT last_name, COUNT(*) FROM people GROUP BY last_name ORDER BY NULL;
+
+SELECT first_name, birthday FROM people WHERE first_name IN ('Camile','John');
+
+SELECT DATEDIFF('1998-04-07'. '1970-03-23')
