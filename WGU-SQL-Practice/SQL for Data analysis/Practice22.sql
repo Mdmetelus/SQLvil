a9526d2ea8330c811d2a2f0e134119fc06b5e95a
Mdmetelus <Mdmetelus@gmail.com>
2019-07-29 16:39:31 -0400

    ...

--- WGU-SQL-Practice/SQL for Data analysis/Practice22.sql
+++ WGU-SQL-Practice/SQL for Data analysis/Practice22.sql
@@ -6,5 +6,20 @@ SELECT * FROM Album WHERE Label = 'Columbia';
 
 SELECT COUNT(*) FROM Album WHERE Label = 'Columbia';
 
+SELECT Name, Population / 1000000 AS PopMM FROM Country
+WHERE Population >= 1000000 ORDER BY Population DESC;
+
+--
+
+USE world;
+
+SELECT 'Hello World';
+
+SELECT 1 + 2;
+
+SELECT * FROM Country;
+
+SELECT * FROM Country ORDER BY Name;
+
 
 
