80e777070ace5ae0c0d4e290e82ff5ea5517fccf
Mdmetelus <Mdmetelus@gmail.com>
2019-08-01 23:35:19 -0400

    ...

--- WGU-SQL-Practice/SQL for Data analysis/Practice22.sql
+++ WGU-SQL-Practice/SQL for Data analysis/Practice22.sql
@@ -113,3 +113,9 @@ SELECT * FROM track;
 
 SELECT * FORM album;
 
+SELECT a.artist AS Artist, a.title AS Album, t.track_number AS 'Track Num', 
+t.title AS Track, t.duration AS Seconds
+FROM album AS a Join track AS t
+ON a.id = t.album_id
+ORDER BY a.artist, a.title, t.track_number;
+
