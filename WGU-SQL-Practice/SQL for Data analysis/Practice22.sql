a36de3be7a0c28490c3e560f93c863c9b2a26a8b
Mdmetelus <Mdmetelus@gmail.com>
2019-08-02 03:24:44 -0400

    ...

--- WGU-SQL-Practice/SQL for Data analysis/Practice22.sql
+++ WGU-SQL-Practice/SQL for Data analysis/Practice22.sql
@@ -127,3 +127,16 @@ SHOW TABLES;
 
 DESCRIBE customer;
 
+USE scratch;
+
+CREATE TABLE test (
+    id INT,
+    name VARCHAR(255),
+    address VARCHAR(255),
+    city VARCHAR(255),
+    state CHAR(2),
+    zip CHAR(10)
+);
+
+DESCRIBE test;
+
