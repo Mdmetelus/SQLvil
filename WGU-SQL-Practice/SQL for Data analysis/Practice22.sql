91256f81b66b279b86b6366b9b8dbea8bf3b7592
Mdmetelus <Mdmetelus@gmail.com>
2019-08-05 19:32:23 -0400

    ...

--- WGU-SQL-Practice/SQL for Data analysis/Practice22.sql
+++ WGU-SQL-Practice/SQL for Data analysis/Practice22.sql
@@ -165,3 +165,14 @@ DESCRIBE numerics;
 
 SELECT * FROM numerics;
 
+SELECT da + db, fa + fb FROM numerics;
+
+SELECT da + db = 0.3 FROM numerics;
+
+SELECT da + db FROM numerics;
+
+SELECT fa + fb FROM numerics;
+
+SELECT fa + fb = 0.3 FROM numerics;
+
+
