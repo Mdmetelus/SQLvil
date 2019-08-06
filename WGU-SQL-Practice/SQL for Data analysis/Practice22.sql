c26e3c6afa38fa72cbc8a6ef0cdb8f2cdc9f8bf9
Mdmetelus <Mdmetelus@gmail.com>
2019-08-06 16:19:03 -0400

    update

--- WGU-SQL-Practice/SQL for Data analysis/Practice22.sql
+++ WGU-SQL-Practice/SQL for Data analysis/Practice22.sql
@@ -246,6 +246,13 @@ INSERT INTO test ( a ) VALUES ('Pablo');
 INSERT INTO test ( a ) VALUES ('Henri');
 INSERT INTO test ( a ) VALUES ('Jackson');
 INSERT INTO test ( a ) VALUES ('Pablo,Jackson,Henri,Henri,Henri');
+
 INSERT INTO test ( a ) VALUES (1);
 INSERT INTO test ( a ) VALUES (2);
 INSERT INTO test ( a ) VALUES (3);
+INSERT INTO test ( a ) VALUES (4);
+INSERT INTO test ( a ) VALUES (5);
+INSERT INTO test ( a ) VALUES (6);
+INSERT INTO test ( a ) VALUES (7);
+
+SELECT * FROM test;
