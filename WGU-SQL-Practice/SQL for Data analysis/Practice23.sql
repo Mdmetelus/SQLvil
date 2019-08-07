0edfa40b7b606c29630e0955b8910bce4b237991
Mdmetelus <Mdmetelus@gmail.com>
2019-08-07 17:20:13 -0400

    ...

--- WGU-SQL-Practice/SQL for Data analysis/Practice23.sql
+++ WGU-SQL-Practice/SQL for Data analysis/Practice23.sql
@@ -102,3 +102,16 @@ CREATE TABLE booltest (a INTEGER, b INTEGER);
 INSERT INTO booltest VALUES (1, 0);
 
 SELECT * FROM booltest;
+
+SELECT
+    CASE WHEN a < 5 THEN 'true' ELSE 'false' END AS boolA,
+    CASE WHEN b > 0 THEN 'true' ELSE 'false' END AS boolB
+    FROM booltest
+;
+
+SELECT
+    CASE WHEN a = 1 THEN 'true' ELSE 'false' END AS boolA,
+    CASE WHEN b = 0 THEN 'true' ELSE 'false' END AS boolB
+    FROM booltest
+;
+
