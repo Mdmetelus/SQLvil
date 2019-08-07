f3411715324fef8d1825ac5ed3d987f27dd78853
Mdmetelus <Mdmetelus@gmail.com>
2019-08-07 03:02:02 -0400

    ...

--- WGU-SQL-Practice/SQL for Data analysis/Practice23.sql
+++ WGU-SQL-Practice/SQL for Data analysis/Practice23.sql
@@ -18,12 +18,22 @@ SELECT 9 >= 7;
 
 SELECT 9 <= 7;
 
-SELECT (9 > 7) AND (12 <27);
+SELECT (9 > 7) AND (12 < 27);
 -- true
 
 SELECT (9 > 7) IS TRUE;
 
-SELECT (9 > 7) OR  (12 <27);
+SELECT (9 > 7) OR  (12 < 27);
  -- true
 
-SELECT 
+SELECT (9 > 7) IS NOT TRUE;
+ -- SELECT (9 > 7) IS FALSE;
+
+SELECT 1 AND 1;
+
+SELECT 1 OR 1;
+
+SELECT 1 OR 0;
+
+SELECT 1 xor 0;
+
