72a482ce195ce3f127985346b7f26152fd9283fd
Mdmetelus <Mdmetelus@gmail.com>
2019-08-07 02:33:22 -0400

    update

--- WGU-SQL-Practice/SQL for Data analysis/Practice23.sql
+++ WGU-SQL-Practice/SQL for Data analysis/Practice23.sql
@@ -10,4 +10,20 @@ SELECT '0.1' > 0;
 
 SELECT '0.1' < 0;
 
+SELECT 0.1 < 0;
+
+SELECT 9 != 7;
+
+SELECT 9 >= 7;
+
+SELECT 9 <= 7;
+
+SELECT (9 > 7) AND (12 <27);
+-- true
+
+SELECT (9 > 7) IS TRUE;
+
+SELECT (9 > 7) OR  (12 <27);
+ -- true
+
 SELECT 
