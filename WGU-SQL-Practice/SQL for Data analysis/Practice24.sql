d5f74f1ab7fb5288ee54f6f8cee6a60c5191a3a9
Mdmetelus <Mdmetelus@gmail.com>
2019-08-11 16:39:29 -0400

    ....

---
+++ WGU-SQL-Practice/SQL for Data analysis/Practice24.sql
@@ -0,0 +1,10 @@
+SELECT LN(2);
+
+SELECT LOG(2);
+
+SELECT LOG(10, 100);  --  log of 1000 to the base of 10; 2;
+
+SELECT LOG(2, 65535);  -- 15.99997798605; to the base 2;
+
+SELECT LOG2(65535);  -- 
+
